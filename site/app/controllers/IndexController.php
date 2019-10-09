2e8dfc05408ea3c69e5e57b6280e87a48ea4d5c3
ilya <wyster@make.im>
2019-10-10 00:09:55 +0200

    feat: лучше использовать оригинальные образы

--- site/app/controllers/IndexController.php
+++ site/app/controllers/IndexController.php
@@ -101,6 +101,9 @@ class IndexController extends \Phalcon\Mvc\Controller
 
     private function sendRequest(string $body): ClientResponse
     {
+        if (!array_key_exists('USERS_CONTAINER_NAME', $_ENV)) {
+            throw new \Exception('Env USERS_CONTAINER_NAME not setted');
+        }
         $provider = $this->getDI()->get(ClientRequest::class);
         $response = $provider->post(
             'http://' . $_ENV['USERS_CONTAINER_NAME'],
